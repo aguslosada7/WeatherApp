074752060f9488dfa3d42d5d6c201694b7f97c89
Agus <agustinalosadamartinez@gmail.com>
2026-06-26 10:08:08 -0300

    Chore: Remove misplaced files from repo restructure

--- shared/src/commonMain/kotlin/weatherapp/data/remote/api/shared/src/commonMain/kotlin/weatherapp/domain/usecase/GetForecastByCityUseCase.kt
+++
@@ -1,9 +0,0 @@
-package weatherapp.data.remote.api.shared.src.commonMain.kotlin.weatherapp.domain.usecase
-
-import weatherapp.domain.model.HourlyForecast
-import weatherapp.domain.repository.WeatherRepository
-
-class GetForecastByCityUseCase(private val repository: WeatherRepository) {
-    suspend operator fun invoke(city: String): Result<List<HourlyForecast>> =
-        repository.getForecastByCity(city)
-}
